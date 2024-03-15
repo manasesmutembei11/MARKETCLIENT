d77ed63738e873b6c534564c4480efc38b5442cb
Lenovo <manasesmutembei11@gmail.com>
2024-03-16 00:13:56 +0300

    initial push

--- src/app/Models/User/user.model.ts
+++ src/app/Models/User/user.model.ts
@@ -1,6 +1,7 @@
 interface User {
     id: number;
-    name: string;
+    firstName: string;
+    lastName: string;
     email: string;
     password: string; // hashed password
     shippingAddress: Address;
