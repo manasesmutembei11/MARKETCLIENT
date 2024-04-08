5b3a96c7e84d505a899267180f3a6cb5453261c6
Lenovo <manasesmutembei11@gmail.com>
2024-04-08 16:57:40 +0300

    commit

--- src/app/Services/blog.service.ts
+++ src/app/Services/blog.service.ts
@@ -18,19 +18,19 @@ export class BlogService {
         return this.http.post<any>(`${this.baseUrl}api/author/CreateAuthor`, Author, {headers: new HttpHeaders({"Content-Type": "application/json"})});
     }
 
-    GetAuthor(id: string) : Observable<any> {
-        return this.http.get<Author>(`${this.baseUrl}api/author/GetAuthor/AuthorId=${id}`)
+    GetAuthor(authorId: string) : Observable<any> {
+        return this.http.get<Author>(`${this.baseUrl}api/author/GetAuthor/AuthorId=${authorId}`)
     }
 
-    UpdateAuthor(id: string, Author: Author) : Observable<Author>{
-        return this.http.put<Author>(`${this.baseUrl}api/author/UpdateAuthor/AuthorId=${id}`, Author)
+    UpdateAuthor(authorId: string, Author: Author) : Observable<Author>{
+        return this.http.put<Author>(`${this.baseUrl}api/author/UpdateAuthor/AuthorId=${authorId}`, Author)
     }
 
     GetPagedAuthors(PageNumber: number, pageSize: number) : Observable<any> {
         return this.http.get<any>(`${this.baseUrl}api/author/GetPagedAuthors?PageNumber=${PageNumber}&PageSize=${pageSize}`)
     }
 
-    DeleteAuthor(id: string) : Observable<void> {
-        return this.http.delete<void>(`${this.baseUrl}api/author/DeleteAuthor?id=${id}`);
+    DeleteAuthor(authorId: string) : Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}api/author/DeleteAuthor?id=${authorId}`);
     }
 }
